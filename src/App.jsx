c1f6309b918b065bdcf8c7af3ed16d21f7401b71
Saugat Shrestha <saugats029@gmail.com>
2023-04-08 20:50:08 +0545

    Fix react error and updated the router package implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import "./app.css";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import "./App.css";
 import Topbar from "./components/topbar/Topbar";
-import Sidebar from "./components/topbar/sidebar/Sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home";
 import NewUser from "./pages/home/newPage/NewUser";
 import User from "./pages/home/user/User";
@@ -11,38 +11,24 @@ import NewProduct from "./pages/newProduct/NewProduct";
 import Product from "./pages/product/Product";
 import ProductList from "./pages/productList/ProductList";
 
-function App() {
-  return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
-  );
+const App = () => {
+    return (
+        <Router>
+            <Topbar/>
+            <div className="container">
+                <Sidebar/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}></Route>
+                    <Route path="/users" element={<UserList/>}></Route>
+                    <Route path="/user/:userId" element={<User/>}></Route>
+                    <Route path="/newUser" element={<NewUser/>}></Route>
+                    <Route path="/products" element={<ProductList/>}></Route>
+                    <Route path="/product/:productId" element={<Product/>}></Route>
+                    <Route path="/newproduct" element={<NewProduct/>}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,36 +1,27 @@
-import {
-  Language,
-  NotificationsNone,
-  Settings,
-} from "@material-ui/icons/NotificationsNone";
 import React from "react";
 import "./topbar.css";
-export default function Topbar() {
-  return (
-    <div className="topbar">
-      <div className="topbarWrapper">
-        <div className="topLeft">
-          <span className="logo">Shuvam Admin</span>
+import {Settings} from "@material-ui/icons";
+
+const TopBar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Shuvam Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <Settings/>
+                    </div>
+                    <img
+                        src="https://images.unsplash.com/photo-1569529465841-dfecdab7503b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                        alt=""
+                        className="topAvatar"
+                    />
+                </div>
+            </div>
         </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
-            <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Settings />
-          </div>
-          <img
-            src="https://images.unsplash.com/photo-1569529465841-dfecdab7503b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-            alt=""
-            className="topAvatar"
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
+
+export default TopBar
